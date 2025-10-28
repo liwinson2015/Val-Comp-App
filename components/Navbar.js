1afb8a01924e7bd6445bec390a4dfa68bed03a09
Winson Li <liwinson2015@gmail.com>
2025-10-28 07:46:44 -0400

    navbar update

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,16 +2,29 @@ export default function Navbar() {
   return (
     <header className="nav-shell">
       <div className="nav-inner">
+        {/* Brand */}
         <div className="brand">
           <span className="brand-main">ValComp</span>
           <span className="brand-sub">5TQ</span>
         </div>
 
+        {/* Navigation Links */}
         <nav className="nav-links">
-          <a href="/" className="nav-link">Home</a>
-          <a href="/valorant" className="nav-link">Valorant</a>
-          <a href="/valorant/bracket" className="nav-link">Bracket</a>
-          <a href="https://discord.gg/yourInviteHere" className="nav-link external">
+          <a href="/" className="nav-link">
+            Home
+          </a>
+          <a href="/valorant" className="nav-link">
+            Valorant
+          </a>
+          <a href="/valorant/bracket" className="nav-link">
+            Bracket
+          </a>
+          <a
+            href="https://discord.gg/yuGpPr6MAa"
+            className="nav-link external"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             Discord
           </a>
         </nav>
