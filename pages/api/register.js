9dad7005191b91405cd0fd13637b2f8f0d0e106e
liwinson2015 <liwinson2015@gmail.com>
2025-10-29 06:57:06 +0000

    redeploy

--- pages/api/register.js
+++ pages/api/register.js
@@ -1,4 +1,4 @@
-import dbConnect from "../../lib/mongodb";
+import { connectToDatabase } from "../../lib/mongodb";
 import Registration from "../../models/Registration";
 
 export default async function handler(req, res) {
@@ -7,7 +7,7 @@ export default async function handler(req, res) {
   }
 
   try {
-    await dbConnect();
+await connectToDatabase();
 
     const { playerName, discordTag, rank, email, tournament } = req.body;
 
