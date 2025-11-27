72d4ae90d0d2d8f5e885102826aa42a2efc9fc9d
Winson Li <liwinson2015@gmail.com>
2025-11-27 02:54:41 -0500

    upcoming to ongoing

--- pages/tournaments/[tournamentId].js
+++ pages/tournaments/[tournamentId].js
@@ -125,7 +125,7 @@ export async function getServerSideProps({ req, params }) {
     }
 
     // Status from Tournament doc (optional for now)
-    const status = tournamentDoc.status || "upcoming";
+    const status = tournamentDoc.status || "ongoing";
 
     return {
       props: {
